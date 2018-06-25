855ddaf7a9c0a35ccc3d817e0f4ef8f3b5876853
Ilker Baltaci <ilkerbaltaci@gmx.at>
2018-06-25 14:26:25 +0200

    Initial commit

---
+++ ImagePicker/String+Localization.swift
@@ -0,0 +1,9 @@
+//
+//  String+Localization.swift
+//  ImagePicker
+//
+//  Created by Ilker Baltaci on 25.06.18.
+//  Copyright © 2018 Ilker Baltaci. All rights reserved.
+//
+
+import Foundation
---
+++ ImagePicker/UIImagePickerPresenting.swift
@@ -0,0 +1,9 @@
+//
+//  UIImagePickerPresenting.swift
+//  ImagePicker
+//
+//  Created by Ilker Baltaci on 25.06.18.
+//  Copyright © 2018 Ilker Baltaci. All rights reserved.
+//
+
+import Foundation
